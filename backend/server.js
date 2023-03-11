c5c25dd488e11fc6b50f5afa7b14d6bc8db3078e
unknown <rafayfarrukh941@gmail.com>
2023-03-11 16:15:46 +0500

    e

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,10 @@ app.get("/", (req, res) => {
   res.send("welcome to backend of Qr code");
 });
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://sync-fusion-client.vercel.app/"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
