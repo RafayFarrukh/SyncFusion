1328a2aded06459351f32a65a21b16f0f1f9df72
unknown <rafayfarrukh941@gmail.com>
2023-03-11 16:17:52 +0500

    e

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
 app.use(function (req, res, next) {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://sync-fusion-client.vercel.app/"
+    "https://sync-fusion-client.vercel.app"
   );
   res.header(
     "Access-Control-Allow-Headers",
